3a2fcacd8e37dd780e3e4f1c2508fe18169ae54a
shahmem <kpmshameemudheenkp@gmail.com>
2024-10-30 10:48:32 +0530

    check time

---
+++ Questions/qstn10.js
@@ -0,0 +1,13 @@
+let time=24;
+if (0<=time & time<=11){
+    console.log("Morning")
+}
+else if(12<=time & time<=17){
+    console.log("Afternoon")
+}
+else if(18<=time & time<=23){
+    console.log("Evening")
+}
+else{
+    console.log("error!")
+}
